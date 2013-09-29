9b8d3b24e6f019d5b588147de10d24ef94a8a9e4
Gabriel Hamdan <ghamdan.eng@gmail.com>
2013-09-29 05:14:45 -0300

    Animated Scroll

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,6 @@ class Person < ActiveRecord::Base
   validates :name,  presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-
+  validates :phone,  :presence => true
   validates :typeuser, :presence => true
 end
