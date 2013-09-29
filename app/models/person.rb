52794c2952fe4233872e2a6fb3897828e4faa902
Gabriel Hamdan <ghamdan.eng@gmail.com>
2013-09-29 02:03:40 -0300

    INIT

---
+++ app/models/person.rb
@@ -0,0 +1,8 @@
+class Person < ActiveRecord::Base
+
+  validates :name,  presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :phone,  :presence => true, length: { maximum: 20 }
+  validates :type, :presence => true
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Iteratrack::Application.config.session_store :cookie_store, key: '_iteratrack_session'
